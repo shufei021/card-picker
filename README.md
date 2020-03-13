c41f3fbe8723ca1b378242ae49c456f10790efdb
unknown <1017981699@qq.com>
2020-03-14 00:21:58 +0800

    picker-card 正式第一版发布

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # picker-card
-一种卡片式的联动选择器
+> 一种卡片式的联动选择器，旨在解决JS多级树结构展现方式，picker-card可以更多的层次结构数据展示和选择。
+
+2020/3/8 第一版
+
+2020/3/13 正式第一版发布
+
+[体验地址:http://moxiaofei.com/picker-card/example/index.html](http://moxiaofei.com/picker-card/example/index.html)
+
+第一次写js插件，难免疏忽大意，大佬莫怪
+
+## 使用
+
+script标签引入方式使用
+
+```html
+<body>
+    <button id="btn">触发按钮</button>	
+</body>    
+<script src="picker-card.min.js"></script>
+<script>
+let list = [{text:'一级-1'.children:[{text:'二级-1-2'}]},{text:'一级-2'}]    
+new PickerCard({
+    trigger:'#btn',
+    list:list,
+    success:function(res,o) {
+        //返回选择项  o对象是最后选择项的对象
+        document.getElementById('btn').innerHTML = res
+        console.log(o)
+    }
+})
+</script>
+```
+
+## 参数配置
+
++ trigger：触发元素，值可以是DOM节点元素，也可以是选择器字符串
++ title：标题
++ list：注入的数据
++ isShowControl：是否显示操作按钮 默认是 false 不显示
++ success：选择完成后的回调
---
+++ example/js/tree-data.js
@@ -0,0 +1,91 @@
+var treeData = [
+    {
+      "text": "第一级1",
+      "orgCode": "第一级编码1",
+      "dataOrg": "01",
+      "children": [
+        {
+          "text": "第二级1",
+          "orgCode": "第二级编码1",
+          "dataOrg": "0101",
+          "children": [
+            {
+              "text": "第三级1",
+              "orgCode": "第三级编码1",
+              "dataOrg": "010101",
+              "children": [
+                {
+                  "text": "第四级1",
+                  "orgCode": "第四级编码1",
+                  "dataOrg": "01010101",
+                  "children": [
+                    
+                  ]
+                },
+                {
+                  "text": "第四级2",
+                  "orgCode": "第四级编码2",
+                  "dataOrg": "01010102",
+                  "children": [
+                    {
+                      "text": "第五级1",
+                      "orgCode": "第五级编码1",
+                      "dataOrg": "010101021",
+                      "children": [
+                        {
+                          "text": "第六级1",
+                          "orgCode": "第六级编码1",
+                          "dataOrg": "0101010211",
+                          "children": [
+                            
+                          ]
+                        }
+                      ]
+                    }
+                  ]
+                },
+              ]
+            },
+          ]
+        },
+        {
+          "text": "第二级2",
+          "orgCode": "第二级编码2",
+          "dataOrg": "0102",
+          "children": [
+            {
+              "text": "第二级1",
+              "orgCode": "第二级编码1",
+              "dataOrg": "02",
+              "children": [
+                
+              ]
+            }
+          ]
+        },
+      ]
+    },
+    {
+      "text": "第一级2",
+      "orgCode": "第一级编码2",
+      "dataOrg": "02",
+      "children": [
+        {
+          "text": "第二级1",
+          "orgCode": "第二级编码1",
+          "dataOrg": "0201",
+          "children": [
+            
+          ]
+        },
+      ]
+    },
+    {
+      "text": "第一级3",
+      "orgCode": "第一级编码3",
+      "dataOrg": "03",
+      "children": [
+        
+      ]
+    },
+];
